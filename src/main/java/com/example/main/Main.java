3c188c8ed613506315b6abd13e26971be0fe5889
andrewhicks123 <80068284+andrewhicks123@users.noreply.github.com>
2021-08-27 16:32:59 -0400

    Initial commit

---
+++ src/main/java/com/example/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Andrew Hicks
+ */
+
+package com.example.main;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.print("What is your name? ");
+        String fname = input.next();
+        System.out.println("Hello, "+fname+ ", nice to meet you!");
+    }
+}
